d375c0f2af0b626b1c6622083c8626c55a7f2819
Julian <jmendezpod@gmail.com>
2025-03-21 23:14:28 +0100

    Primer commit: MVP básico de la app de tareas

---
+++ src/main/java/com/example/taskmanager/TaskController.java
@@ -0,0 +1,23 @@
+package com.example.taskmanager;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+    @Autowired
+    private TaskRepository taskRepository;
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    @PostMapping
+    public Task createTask(@RequestBody Task task) {
+        return taskRepository.save(task);
+    }
+}
