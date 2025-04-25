283631b83453341db73cdb5e654a756c02669a4d
julian-modesto.mendez-podadera <julian-modesto.mendez-podadera@t-systems.com>
2025-04-25 12:07:04 +0200

    Added app y projecto android studio con webviewer

--- frontend/android/app/src/main/java/com/example/frontend/MainActivity.java
+++
@@ -1,5 +0,0 @@
-package com.example.frontend;
-
-import com.getcapacitor.BridgeActivity;
-
-public class MainActivity extends BridgeActivity {}
---
+++ frontend/androidstudioproject/MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.AndroidView
+import com.example.myapplication.ui.theme.MyApplicationTheme
+import android.webkit.WebView
+import androidx.compose.ui.viewinterop.AndroidView
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MyApplicationTheme {
+                Surface(modifier = Modifier.fillMaxSize()) {
+                    WebViewComposable()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun WebViewComposable() {
+    AndroidView(
+        factory = { context ->
+            WebView(context).apply {
+                settings.javaScriptEnabled = true
+                loadUrl("http://34.175.108.172/")
+            }
+        },
+        modifier = Modifier.fillMaxSize()
+    )
+}
